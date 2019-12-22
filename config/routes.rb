61ca9c737a10eb830f135d99f036791d30fdb8d3
Vinh <theten12@gmail.com>
2019-12-23 00:03:50 +0700

    nested attributes for Picture & Dynamic form for Picture

--- config/routes.rb
+++ config/routes.rb
@@ -23,9 +23,10 @@ Rails.application.routes.draw do
         get :following, :followers
       end
     end
-    resources :account_activations, only: [:edit]
-    resources :password_resets,     only: [:new, :create, :edit, :update]
-    resources :microposts,          only: [:create, :destroy]
-    resources :relationships,       only: [:create, :destroy]
   end
+
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
