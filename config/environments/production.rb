1a7aa22d97b08da82653f377f4063e38241c5eb7
Vinh <theten12@gmail.com>
2019-12-22 14:02:36 +0700

    heroku app url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'safe-journey-83480.herokuapp.com/'
+  host = 'lit-gorge-47261.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
